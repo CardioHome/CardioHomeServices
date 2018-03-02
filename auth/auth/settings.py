3c26aed91e3aefc30dceb81f98f831f959d3313f
David Fung <dtyfung@uw.edu>
2018-03-01 19:21:04 -0800

    Changed project database settings for PostgreSQL.

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CardioHomeAuthDB',
+        'USER': 'CardioHomeUser',
+        'PASSWORD': 'qwertyuiop',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- hippa/hippa/settings.py
+++ hippa/hippa/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'hippa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CardioHomeHIPPADB',
+        'USER': 'CardioHomeUser',
+        'PASSWORD': 'qwertyuiop',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
